98c77fedb92cb07da3ec6198136ce84bc2344657
nil <nil>
2020-01-06 12:34:21 +1100

    Added tests to utils

--- src/util.rs
+++ src/util.rs
@@ -45,3 +45,98 @@ pub fn contains_item_label(label: &String, items: &Vec<Item>) -> bool {
 
     return false;
 }
+
+#[cfg(test)]
+mod test {
+    #[test]
+    pub fn test_is_base_32() {
+        use super::*;
+        assert!(is_base_32(&String::from("test")));
+    }
+
+    #[test]
+    pub fn test_is_base_32_fail() {
+        use super::*;
+        assert!(!is_base_32(&String::from("abc123")));
+    }
+
+    #[test]
+    pub fn test_is_base_32_c() {
+        use super::*;
+        assert!(is_base_32_c('c'));
+    }
+
+    #[test]
+    pub fn test_is_base_32_c_fail() {
+        use super::*;
+        assert!(!is_base_32_c('1'));
+    }
+
+    #[test]
+    pub fn test_is_number() {
+        use super::*;
+        assert!(is_number(&String::from("123")));
+    }
+
+    #[test]
+    pub fn test_is_number_fail() {
+        use super::*;
+        assert!(!is_number(&String::from("123a")));
+    }
+
+    #[test]
+    pub fn test_contains_white_space() {
+        use super::*;
+        assert!(!contains_white_space(&String::from("123")));
+    }
+
+    #[test]
+    pub fn test_contains_white_space_fail_space() {
+        use super::*;
+        assert!(contains_white_space(&String::from("1 23a")));
+    }
+
+    #[test]
+    pub fn test_contains_white_space_fail_tab() {
+        use super::*;
+        assert!(contains_white_space(&String::from("1\t23a")));
+    }
+
+    #[test]
+    pub fn test_contains_item_label() {
+        use super::*;
+        use crate::Digits;
+        let items = vec![Item{
+            label: String::from("test1"),
+            secret: String::from("test"),
+            digits: Digits::Six,
+            split_time: 30,
+        },Item{
+            label: String::from("test2"),
+            secret: String::from("test2"),
+            digits: Digits::Six,
+            split_time: 30,
+        }];
+
+        assert!(contains_item_label(&String::from("test1"), &items));
+    }
+
+    #[test]
+    pub fn test_contains_item_label_fail() {
+        use super::*;
+        use crate::Digits;
+        let items = vec![Item{
+            label: String::from("test1"),
+            secret: String::from("test"),
+            digits: Digits::Six,
+            split_time: 30,
+        },Item{
+            label: String::from("test2"),
+            secret: String::from("test2"),
+            digits: Digits::Six,
+            split_time: 30,
+        }];
+
+        assert!(!contains_item_label(&String::from("test3"), &items));
+    }
+}
